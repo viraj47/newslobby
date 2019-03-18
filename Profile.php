0fd58dec8a9672f804037c2ab7e208265a35c376
viraj47 <32589817+viraj47@users.noreply.github.com>
2019-03-18 23:07:07 +0530

    Update Profile.php

--- Profile.php
+++ Profile.php
@@ -13,6 +13,9 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="css/profile.css">
+  <script src="barba.min.js"></script>
+  <script type="text/javascript" src="scripts/barbapageload.js"></script>
   <script src="barba.min.js"></script>
 </head>
 <body>
